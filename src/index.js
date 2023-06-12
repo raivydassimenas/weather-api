08171c88679204a30b7b0b8b5a96080cde125dba
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2023-06-12 16:06:34 +0300

    Final version.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import _ from 'lodash';
 import './style.css';
 
-const API_KEY = process.env.API_KEY;
-
 const fetchWeather = async (loc) => {
-    let weatherData = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${loc}`);
+    let weatherData = await fetch(`https://api.weatherapi.com/v1/current.json?key=3f0918a66dae40e09f270450232304&q=${loc}`);
     weatherData = await weatherData.json();
 
     const { location: {name}, current: {condition: {text}, temp_c, humidity }} = weatherData;
 
-    weatherData = { name, temp_c, humidity, condition: text };
+    weatherData = { name, temp_c, humidity, text };
 
-    console.log(weatherData);
+    return weatherData
 }
 
-fetchWeather("London");
+const weatherForm = document.querySelector("#weather");
+const weatherDataDiv = document.querySelector("#weather-data");
+console.log(weatherDataDiv);
+
+weatherForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const location = weatherForm.elements["location"];
+
+    const weatherData = await fetchWeather(location.value);
+
+    if (weatherData) {
+        weatherDataDiv.innerHTML = '';
+        weatherDataDiv.classList.add('weather-info-container');
+
+        const locatiionH2 = document.createElement("h2");
+        locatiionH2.innerHTML = "Location: " + weatherData.name;
+        weatherDataDiv.appendChild(locatiionH2);
+
+        const tempP = document.createElement("p");
+        tempP.innerHTML = "Temperature (C): " + weatherData.temp_c;
+        weatherDataDiv.appendChild(tempP);
+
+        const humidityP = document.createElement("p");
+        humidityP.innerHTML = "Humidity: " + weatherData.humidity;
+        weatherDataDiv.appendChild(humidityP);
+
+        const conditionP = document.createElement("p");
+        conditionP.innerHTML = "Condition: " + weatherData.text;
+        weatherDataDiv.appendChild(conditionP);
+    } else {
+        weatherDataDiv.innerHTML = '';
+        weatherDataDiv.classList = [];
+        alert("Invalid location");
+    }
+
+});
